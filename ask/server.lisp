94c228a47b25d094dd3c1b047d5b626e34a26db7
Ben McGunigle <bnmcgn@gmail.com>
2015-06-04 15:16:10 -0700

    Ask-store operational

--- ask/server.lisp
+++ ask/server.lisp
@@ -30,27 +30,6 @@
 	    (setf res (treeproc code))))
       (values res qs syms))))
 
-
-(defun %create-input-processor (qs names)
-  (let ((validators
-	 (collecting-hash-table (:mode :replace)
-	   (loop for q in qs
-		for n in names
-		do (collect n (%q-validator q))))))
-    (lambda (data storage)
-      (multiple-value-bind (good errors)
-	  (with-collectors (g< e<)
-	    (dolist (n names)
-	      (awhen (assoc n data :test #'equal)
-		(aif2only
-		 (funcall (gethash n validators) (cdr it))
-		 (g< (cons n it))
-		 (e< (cons n it))))))
-	(if errors
-	    errors
-	    (dolist (itm good)
-	      (setf (gethash (first itm) storage) (cons (cdr itm) t))))))))
-
 (defun %prep-q-dispatch (q name prev-val)
   (list (cons :next (plist-hash-table (list name 
     (plist-hash-table
@@ -69,44 +48,42 @@
   (hash-table-keys (assoc-cdr :next disp)))
 
 (defun create-ask-manager (code qs names)
-  (with-gensyms (stor continuations dispatch inproc)
-    `(let ((,stor (make-hash-table))
-	   (,continuations nil)
+  (with-gensyms (continuations dispatch stor)
+    `(let ((,continuations nil)
 	   (,dispatch nil)
-	   (,inproc (%create-input-processor 
-		     (list ,@(mapcar #'%unquote-q qs)) ',names)))
-       (labels ((exists-answer (name)
-		  (nth-value 1 (gethash name ,stor)))
-		(answer (name)
-		  (car (gethash name ,stor))))
+	   (,stor (make-instance 'ask-store  
+			 :q-clauses (list ,@(mapcar #'%unquote-q qs))
+			 :names ',names)))
+       (flet ((%%answer (thing)
+		(answer ,stor thing :translate t)))
 	 (macrolet ((a (itm)
-		      `(answer ,(assoc-cdr itm ',(pairlis (mapcar #'second qs)
-							  names)))))
+		      `(%%answer ',itm)))
 	   (cl-cont:with-call/cc
 	     (labels 
 		 ((display (name)
 		    (let ((q (assoc-cdr name ',(pairlis names qs))))
 		      (setf ,dispatch 
-			    (%prep-q-dispatch q name (and (exists-answer name)
-							  (a name))))
+			    (%prep-q-dispatch q name 
+					      (and (exists-answer ,stor name)
+						   (answer ,stor name))))
 		      (cl-cont:let/cc k
 			(push k ,continuations))
-		      (answer name))))
+		      (answer ,stor name))))
 	       ,@code
-	       (setf ,dispatch '((:success . t))))))
-	 (lambda (data)
-	   (aif (funcall ,inproc data ,stor)
-		(progn
-		  (print data)
-		  (print `((:error . ,it))))
-		(values
-		 (progn
-		   (when (with-any/all/none
-			   (dolist (keyname (%dispatch-keys ,dispatch))
-			     (all (exists-answer keyname))) t)
-		     (funcall (car ,continuations)))
-		   ,dispatch)
-		 ,stor)))))))
+	       (setf ,dispatch '((:success . t)))))))
+       (lambda (data)
+	 (aif (add-input ,stor data)
+	      (progn
+		(print data)
+		(print `((:error . ,it))))
+	      (values
+	       (progn
+		 (when (with-any/all/none
+			 (dolist (keyname (%dispatch-keys ,dispatch))
+			   (all (exists-answer ,stor keyname))) t)
+		   (funcall (car ,continuations)))
+		 ,dispatch)
+	       ,stor))))))
 
 (defun register-ask-manager (aman &key (session *session*))
   (unless (gethash :askstore session)
