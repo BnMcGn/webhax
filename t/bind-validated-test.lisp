1cbe299d50d100d5ef684342ec681ce51732ad30
Ben McGunigle <bnmcgn@gmail.com>
2015-06-17 20:14:51 -0700

    Tests working

--- t/bind-validated-test.lisp
+++ t/bind-validated-test.lisp
@@ -1,8 +1,7 @@
 
-(def-package 
-
 (in-package #:webhax-test)
 
+(in-suite webhax-test)
 
 (defvar input
   '(("one" "2" "3")
@@ -16,13 +15,20 @@
   `(let ((webhax::*input-normalize* #'test-input-normalize))
      ,@body))
 
-(twrap
-  (bind-validated-input 
-      (input
-       (item1 (ratify-wrapper :overlength))
-       (item2 (ratify-wrapper :integer))
-       (item3 (ratify-wrapper :integer) :optional t)
-       (a (ratify-wrapper :overlength) :key t)
-       (b (ratify-wrapper :email) :key t :multiple t)
-       ((d 5) (ratify-wrapper :integer) :key t))
-    (list item1 item3 item2 d b a)))
+(defun testfunc1 ()
+  (twrap
+    (bind-validated-input 
+	(input
+	 (item1 (ratify-wrapper :overlength))
+	 (item2 (ratify-wrapper :integer))
+	 (item3 (ratify-wrapper :integer) :optional t)
+	 (a (ratify-wrapper :overlength) :key t)
+	 (b (ratify-wrapper :email) :key t :multiple t)
+	 ((d 5) (ratify-wrapper :integer) :key t))
+      (list item2 item3 d item1 b a))))
+
+(test bind-validated-input1
+  (is (= 10
+	 (reduce #'+ (remove-if-not #'integerp (testfunc1)))))
+  (is (= 2 (length (fifth (testfunc1)))))
+  (is (every #'stringp (fifth (testfunc1)))))
---
+++ t/package.lisp
@@ -0,0 +1,6 @@
+
+(defpackage #:webhax-test
+    (:use #:cl #:webhax #:fiveam #:webhax-validate))
+
+(in-package :webhax-test)
+(def-suite webhax-test)
