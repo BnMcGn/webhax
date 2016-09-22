091073e1ac84db13ff2876a41add6a7bd6338f6f
bnmcgn@gmail.com <bnmcgn@gmail.com>
2016-09-22 12:50:39 -0700

    Only get existing answers in %ask-answers

--- ask/client.lisp
+++ ask/client.lisp
@@ -43,8 +43,9 @@
        (defun %ask-answers (data fieldspecs)
          (let ((res (-object)))
            (ps-gadgets:do-keyvalue (k fspec fieldspecs)
-             (setf (getprop res (@ fspec 'real-name))
-                   (getprop data k)))
+             (when (chain data (has-own-property k))
+               (setf (getprop res (@ fspec 'real-name))
+                     (getprop data k))))
            res))
 
        (def-component ask-main
